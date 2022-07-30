f0c90470b5e3a8b6b292899eb7a697d05f8029b4
Dylan Morrison <dylanjmorrison30@gmail.com>
2022-07-30 21:11:03 +0100

    react added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
 let notes = [
   {
@@ -26,10 +27,6 @@ let notes = [
     important: true,
   },
 ];
-
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
